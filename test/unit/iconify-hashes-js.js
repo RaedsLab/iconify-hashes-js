d3f4f8469bd4d171351c7bd1ccb2f1bfd03136b2
Raed Chammam <raed.chammam@gmail.com>
2017-01-07 23:42:50 +0100

    dummy test

--- test/unit/iconify-hashes-js.js
+++ test/unit/iconify-hashes-js.js
@@ -2,4 +2,16 @@ import iconify from '../../src/iconify-hashes-js';
 
 describe('iconify', () => {
 
+  describe('Hash function', () => {
+    beforeEach(() => {
+      spy(iconify, 'normalize');
+      iconify.getIcons('abc');
+    });
+
+    it('should have been run once', () => {
+      expect(iconify.normalize).to.have.been.calledOnce;
+    });
+
+  });
+
 });
