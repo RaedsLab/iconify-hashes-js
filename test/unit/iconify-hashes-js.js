293d6b4f4d9eadf699c674eb3932d74e699aa38f
Raed Chammam <raed.chammam@gmail.com>
2017-01-14 13:20:53 +0100

    added tests

--- test/unit/iconify-hashes-js.js
+++ test/unit/iconify-hashes-js.js
@@ -12,6 +12,19 @@ describe('iconify', () => {
       expect(iconify.normalize).to.have.been.calledOnce;
     });
 
+    it('should have color & form keys', () => {
+      const icons = iconify.iconify('abc');
+      icons.forEach(icon => {
+        expect(icon).to.have.all.keys('color', 'form');
+      });
+    });
+
+    it('should be string', () => {
+      const icons = iconify.getIcons('abc');
+      expect(icons).to.be.a('string');
+
+    });
+
   });
 
 });
